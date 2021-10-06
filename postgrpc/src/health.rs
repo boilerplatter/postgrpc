6b46f053565e22e1ce49d2d4f9f05eadaba310b1
Alex Pearson <alex@alexpear.com>
2021-10-06 15:58:41 -0400

    🏁 Set up health checks for specific services

--- postgrpc/src/health.rs
+++ postgrpc/src/health.rs
@@ -2,46 +2,52 @@ use crate::proto::health::{
     health_check_response::ServingStatus, health_server::Health as GrpcService, HealthCheckRequest,
     HealthCheckResponse,
 };
-use futures_util::{pin_mut, stream, StreamExt, TryFutureExt};
+use futures_util::{pin_mut, stream, StreamExt};
 use postgres_pool::{Connection, Pool};
-use std::{sync::Arc, time::Duration};
+use std::{hash::Hash, sync::Arc, time::Duration};
+use tokio::sync::mpsc::error::SendError;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use tonic::{Request, Response, Status};
 
 pub struct Health<P>
 where
     P: Pool,
-    P::Key: Default,
+    P::Key: Hash + Eq + Default + Clone,
 {
     pool: Arc<P>,
+    #[cfg(feature = "transaction")]
+    transactions: postgres_transaction_pool::Pool<P>,
+}
+
+impl<P> Clone for Health<P>
+where
+    P: Pool,
+    P::Key: Hash + Eq + Default + Clone,
+{
+    fn clone(&self) -> Self {
+        Self {
+            pool: Arc::clone(&self.pool),
+            #[cfg(feature = "transaction")]
+            transactions: self.transactions.clone(),
+        }
+    }
 }
 
 impl<P> Health<P>
 where
-    P: Pool,
-    P::Key: Default,
+    P: Pool + 'static,
+    P::Key: Hash + Eq + Default + Clone + Send + Sync,
+    <P::Connection as Connection>::Error: Send + Sync,
 {
     pub fn new(pool: Arc<P>) -> Self {
-        Self { pool }
+        Self {
+            #[cfg(feature = "transaction")]
+            transactions: postgres_transaction_pool::Pool::new(Arc::clone(&pool)),
+            pool,
+        }
     }
-}
 
-#[tonic::async_trait]
-impl<P> GrpcService for Health<P>
-where
-    P: Pool + 'static,
-    P::Key: Send + Sync + Default,
-{
-    #[tracing::instrument(skip(self))]
-    async fn check(
-        &self,
-        _request: Request<HealthCheckRequest>,
-    ) -> Result<Response<HealthCheckResponse>, Status> {
-        tracing::debug!("Health check received");
-
-        // use the default value of the given key
-        let key = P::Key::default();
-
+    async fn check_postgres_service(&self, key: P::Key) -> Result<(), Status> {
         // attempt to get a connection from the pool
         let connection = self
             .pool
@@ -55,6 +61,76 @@ where
             .await
             .map_err(|error| Status::unavailable(error.to_string()))?;
 
+        Ok(())
+    }
+
+    #[cfg(feature = "transaction")]
+    async fn check_transaction_service(&self, key: P::Key) -> Result<(), Status> {
+        // attempt to start a transaction
+        let id = self
+            .transactions
+            .begin(key.clone())
+            .await
+            .map_err(|error| Status::unavailable(error.to_string()))?;
+
+        let transaction_key = postgres_transaction_pool::Key::new(key.clone(), id);
+
+        // attempt to retrieve the active transaction
+        let transaction = self
+            .transactions
+            .get_connection(transaction_key)
+            .await
+            .map_err(|error| Status::unavailable(error.to_string()))?;
+
+        // attempt to make a simple query against the transaction
+        transaction
+            .query("SELECT 1", &[])
+            .await
+            .map_err(|error| Status::unavailable(error.to_string()))?;
+
+        // attempt to roll the transaction back
+        self.transactions
+            .rollback(id, key)
+            .await
+            .map_err(|error| Status::unavailable(error.to_string()))?;
+
+        Ok(())
+    }
+}
+
+#[tonic::async_trait]
+impl<P> GrpcService for Health<P>
+where
+    P: Pool + 'static,
+    P::Key: Hash + Eq + Default + Clone + Send + Sync,
+    <P::Connection as Connection>::Error: Send + Sync,
+{
+    #[tracing::instrument(skip(self))]
+    async fn check(
+        &self,
+        request: Request<HealthCheckRequest>,
+    ) -> Result<Response<HealthCheckResponse>, Status> {
+        // use the default value of the given key
+        let key = P::Key::default();
+
+        // parse the service from the request
+        match request.into_inner().service.to_lowercase().as_str() {
+            "" => {
+                #[cfg(feature = "transaction")]
+                self.check_transaction_service(key.clone()).await?;
+                self.check_postgres_service(key).await?;
+            }
+            "postgres" => self.check_postgres_service(key).await?,
+            #[cfg(feature = "transaction")]
+            "transaction" => self.check_transaction_service(key).await?,
+            service => {
+                return Err(Status::not_found(format!(
+                    "Service '{}' does not exist",
+                    service
+                )))
+            }
+        };
+
         Ok(Response::new(HealthCheckResponse {
             status: ServingStatus::Serving.into(),
         }))
@@ -65,33 +141,23 @@ where
     #[tracing::instrument(skip(self))]
     async fn watch(
         &self,
-        _request: Request<HealthCheckRequest>,
+        request: Request<HealthCheckRequest>,
     ) -> Result<Response<Self::WatchStream>, Status> {
-        tracing::debug!("Health check stream started");
-
-        // get a reference to the pool
-        let pool = Arc::clone(&self.pool);
-
+        // set up streamable clones of health check components
+        let health_service = self.clone();
+        let request = request.into_inner();
         let count = 1;
+
+        // unfold an infinite stream of health checks
         let watch_stream = stream::unfold(count, move |count| {
-            let key = P::Key::default();
-            let pool = Arc::clone(&pool);
+            let health_service = health_service.clone();
+            let request = Request::new(request.clone());
 
             async move {
-                // attempt to make a simple query against the pool
-                let response = pool
-                    .get_connection(key)
-                    .map_err(|error| Status::unavailable(error.to_string()))
-                    .and_then(|connection| async move {
-                        connection
-                            .query("SELECT 1", &[])
-                            .await
-                            .map_err(|error| Status::unavailable(error.to_string()))
-                    })
+                let response = health_service
+                    .check(request)
                     .await
-                    .map(|_| HealthCheckResponse {
-                        status: ServingStatus::Serving.into(),
-                    });
+                    .map(|response| response.into_inner());
 
                 Some((response, count + 1))
             }
@@ -109,7 +175,7 @@ where
                 tokio::time::sleep(Duration::from_secs(1)).await;
             }
 
-            Ok::<_, tokio::sync::mpsc::error::SendError<_>>(())
+            Ok::<_, SendError<_>>(())
         });
 
         Ok(Response::new(UnboundedReceiverStream::new(receiver)))
